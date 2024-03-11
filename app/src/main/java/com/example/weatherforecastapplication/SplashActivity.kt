b17d03000a8799b4c71a1d71871999bc0a11ccfa
HabibaHagras <habibahagras22@gmail.com>
2024-03-11 15:43:56 +0200

    first commit

---
+++ app/src/main/java/com/example/weatherforecastapplication/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.weatherforecastapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import com.airbnb.lottie.LottieAnimationView
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var lottieAnimationView: LottieAnimationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        lottieAnimationView = findViewById(R.id.animationView)
+        Handler().postDelayed({
+            Log.i("TAG", "SplashActivity")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 9000)
+
+    }
+}
